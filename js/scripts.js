9306a6778a6e0ab86555a54346966586aa55635a
Robert Bunch <rob@digitalcrafts.com>
2017-10-05 11:36:20 -0400

    Finish layout markup

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-console.log('sanity check');
+// console.log('sanity check');
+$(document).ready(function(){
+	// Wait for the DOM JS...
+	BlackJack deal function
+		Create deck function
+		Shuffle deck function
+		Add card[0] and card[2] to player hand, 1 and 3 to dealer
+		Place card function
+		Push card onto players array
+
+});
